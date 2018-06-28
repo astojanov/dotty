ba9739d2ca1e4bb156cb438d9108906743ca0e9c
Martin Duhem <martin.duhem@gmail.com>
2018-06-28 16:45:33 +0200

    Add tests for Dottydoc from tasty

--- doc-tool/test/SimpleComments.scala
+++ doc-tool/test/SimpleComments.scala
@@ -1,21 +1,30 @@
 package dotty.tools
 package dottydoc
 
+import dotc.util.SourceFile
+
 import org.junit.Test
 import org.junit.Assert._
 
-class TestSimpleComments extends DottyDocTest {
+class SimpleCommentsFromSourceTest extends SimpleCommentsBase with CheckFromSource
+class SimpleCommentsFromTastyTest extends SimpleCommentsBase with CheckFromTasty
+
+abstract class SimpleCommentsBase extends DottyDocTest {
 
   @Test def simpleComment = {
-    val source =
+    val source = new SourceFile(
+      "HelloWorld.scala",
       """
       |package scala
       |
       |/** Hello, world! */
       |trait HelloWorld
       """.stripMargin
+    )
 
-    checkSource(source) { packages =>
+    val className = "scala.HelloWorld"
+
+    check(className :: Nil, source :: Nil) { packages =>
       val traitCmt =
         packages("scala")
         .children.find(_.path.mkString(".") == "scala.HelloWorld")
